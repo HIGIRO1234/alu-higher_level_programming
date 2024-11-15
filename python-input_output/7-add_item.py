4af01a882d76a6d5ed16811105c7e393188e3122
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-15 05:08:44 -0800

    mm

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""write script that adds to python list"""
+import sys
+from os.path import exists
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 filename = "add_item.json"
 
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
+# Check if the file exists
+if exists(filename):
+    # Load existing data from the file
+    items = load_from_json_file(filename)
+else:
+    # Initialize an empty list if the file doesn't exist
+    items = []
 
-for i in argv[1:]:
-    json_list.append(argv)
+# Add all command-line arguments to the list
+items.extend(sys.argv[1:])
 
-save_to_json_file(json_list, filename)
+# Save the updated list back to the file
+save_to_json_file(items, filename)
