4b4c17300e1cdbf7958ced0a6754664846701b01
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-18 01:05:26 -0800

    commit

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -3,7 +3,7 @@
 -- Results are sorted in descending order by rating
 
 SELECT tv_shows.title, 
-       SUM(tv_show_ratings.score) AS rating
+       SUM(tv_show_ratings.rating_value) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
 GROUP BY tv_shows.title
