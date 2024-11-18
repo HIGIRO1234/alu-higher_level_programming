29acfa6a70ec6c6de54660990b25adecdb97463a
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-18 00:30:52 -0800

    commit

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,11 @@
--- Lists all shows from hbtn_0d_tvshows that don't have a linked genre
-SELECT tv_shows.title
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT DISTINCT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL 
+WHERE tv_shows.id NOT IN (
+    SELECT tv_shows.id
+    FROM tv_shows
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY tv_shows.title;
