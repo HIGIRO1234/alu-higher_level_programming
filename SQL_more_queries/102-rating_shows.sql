df9fac579e5d98a35d9aa5e20277d421a50430ad
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-18 01:01:49 -0800

    commti

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,11 @@
+-- Script to list all TV shows by their total rating
+-- Each record displays: tv_shows.title - rating sum
+-- Results are sorted in descending order by rating
+
 SELECT tv_shows.title, 
-       SUM(tv_show_ratings.score) AS rating
+       SUM(tv_show_ratings.rating) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
+
