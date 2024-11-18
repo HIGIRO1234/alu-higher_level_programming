ffceb25b734301dc061ce335c344022d398f4656
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-18 00:33:40 -0800

    commit

---
+++ SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- File: 102-rating_shows.sql
+SELECT tv_shows.title, 
+       SUM(tv_show_ratings.rating) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
+
