abaf339695b658b69dddb8b3d725f23cef564fd1
HIGIRO1234 <l.higiro@alustudent.com>
2024-11-18 00:22:08 -0800

    commit

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,6 @@
--- Lists all genres not linked to the show Dexter
-SELECT DISTINCT name
-FROM tv_genres
-WHERE tv_genres.id NOT IN (
-    SELECT tv_genres.id
-    FROM tv_genres
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter'
-)
-ORDER BY name;
+-- Lists all shows from hbtn_0d_tvshows that don't have a linked genre
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL 
+ORDER BY tv_shows.title;
